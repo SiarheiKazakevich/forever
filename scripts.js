5e8a911b4dd61ebf9fd85bd64ef5b3aab3e4efcb
Siarhei Kazakevich <dianrom@tut.by>
2025-01-04 05:50:26 +0300

    refactor: change titles in music list

--- scripts.js
+++ scripts.js
@@ -21,16 +21,16 @@ const playlist = document.getElementById('playlist');
 // Список аудиофайлов
 const audioFiles = [
   { title: 'Часть 0', src: './assets/audio/ForeverN0.m4a' },
-  { title: 'Часть 1', src: './assets/audio/ForeverN1.m4a' },
+  { title: 'Часть 1,2', src: './assets/audio/ForeverN1.m4a' },
   { title: 'Часть 3', src: './assets/audio/ForeverN3.m4a' },
   { title: 'Часть 4', src: './assets/audio/ForeverN4.m4a' },
-  { title: 'Часть 5', src: './assets/audio/ForeverN5.m4a' },
-  { title: 'Часть 8', src: './assets/audio/ForeverN8.m4a' },
-  { title: 'Часть 10', src: './assets/audio/ForeverN10.mp3' },
-  { title: 'Часть 12', src: './assets/audio/ForeverN12.mp3' },
+  { title: 'Часть 5,6,7', src: './assets/audio/ForeverN5.m4a' },
+  { title: 'Часть 8,9', src: './assets/audio/ForeverN8.m4a' },
+  { title: 'Часть 10,11', src: './assets/audio/ForeverN10.mp3' },
+  { title: 'Часть 12,13,14,15', src: './assets/audio/ForeverN12.mp3' },
   { title: 'Часть 16', src: './assets/audio/ForeverN16.mp3' },
-  { title: 'Часть 17', src: './assets/audio/ForeverN17.mp3' },
-  { title: 'Часть 19', src: './assets/audio/ForeverN19.mp3' },
+  { title: 'Часть 17,18', src: './assets/audio/ForeverN17.mp3' },
+  { title: 'Часть 19,20', src: './assets/audio/ForeverN19.mp3' },
   { title: 'Часть 21', src: './assets/audio/ForeverN21.mp3' }
 ];
 
